1a1048d51af16de0015c9dd0d7bfb42f674d64e2
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-09-28 02:01:08 +0700

    Remove ActorProvider | Minor Changes

--- Jellyfin.Plugin.PhoenixAdult/Core/ExternalId.cs
+++ Jellyfin.Plugin.PhoenixAdult/Core/ExternalId.cs
@@ -1,4 +1,3 @@
-using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
@@ -33,30 +32,4 @@ namespace PhoenixAdult
         public string UrlFormatString
             => null;
     }
-/*
-    public class PhoenixAdultActorExternalId : IExternalId
-    {
-#if __EMBY__
-        public string Name => Plugin.Instance.Name;
-#else
-        public string ProviderName => Plugin.Instance.Name;
-#endif
-
-        public bool Supports(IHasProviderIds item)
-            => item is Person;
-
-#if __EMBY__
-
-#else
-        public ExternalIdMediaType? Type
-            => ExternalIdMediaType.Person;
-#endif
-
-        public string Key
-            => Plugin.Instance.Name + "Actor";
-
-        public string UrlFormatString
-            => null;
-    }
-*/
 }
--- Jellyfin.Plugin.PhoenixAdult/Helpers/Utils/Database.cs
+++ Jellyfin.Plugin.PhoenixAdult/Helpers/Utils/Database.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using PhoenixAdult;
 
