fe0621ea20e551a5829b552550e5588c3f16b7ae
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-10-26 01:57:38 +0700

    Update sitelist | Minor changes

--- Jellyfin.Plugin.PhoenixAdult/Sites/NetworkNubiles.cs
+++ Jellyfin.Plugin.PhoenixAdult/Sites/NetworkNubiles.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
