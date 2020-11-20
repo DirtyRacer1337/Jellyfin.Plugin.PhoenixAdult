0c553747c77507d14003bfb18a4d4b1cbf10a744
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-11-21 04:32:40 +0700

    Minor changes

--- Jellyfin.Plugin.PhoenixAdult/Sites/SiteHegre.cs
+++ Jellyfin.Plugin.PhoenixAdult/Sites/SiteHegre.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using MediaBrowser.Controller.Entities;
