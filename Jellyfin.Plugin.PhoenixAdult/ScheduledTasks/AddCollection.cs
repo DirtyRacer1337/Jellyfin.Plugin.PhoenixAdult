b59a954d26d18a27a7ce55344a71ec2ced5b86de
DirtyRacer1337 <DirtyRacer@ya.ru>
2021-04-17 04:10:40 +0700

    Add collection task

---
+++ Jellyfin.Plugin.PhoenixAdult/ScheduledTasks/AddCollection.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Collections;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Tasks;
+
+namespace PhoenixAdult.ScheduledTasks
+{
+    public class AddCollection : IScheduledTask
+    {
+        private readonly ILibraryManager libraryManager;
+
+        private readonly ICollectionManager collectionManager;
+
+        public AddCollection(ILibraryManager libraryManager, ICollectionManager collectionManager)
+        {
+            this.libraryManager = libraryManager;
+            this.collectionManager = collectionManager;
+        }
+
+        public string Key => Plugin.Instance.Name + "AddCollection";
+
+        public string Name => "Add Collection";
+
+        public string Description => "Creates Collection for every scene";
+
+        public string Category => Plugin.Instance.Name;
+
+        public async Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
+        {
+            await Task.Yield();
+            progress?.Report(0);
+
+            var items = this.libraryManager.GetItemList(new InternalItemsQuery()).Where(o => o.ProviderIds.ContainsKey(Plugin.Instance.Name));
+
+            var studios = items.SelectMany(o => o.Studios).Distinct().ToList();
+
+            foreach (var (idx, studio) in studios.WithIndex())
+            {
+                progress?.Report((double)idx / studios.Count * 100);
+
+                var movies = items.Where(o => o.Studios.Contains(studio, StringComparer.OrdinalIgnoreCase));
+                var option = new CollectionCreationOptions
+                {
+                    Name = studio,
+#if __EMBY__
+                    ItemIdList = movies.Select(o => o.InternalId).ToArray(),
+#else
+                    ItemIdList = movies.Select(o => o.Id.ToString()).ToArray(),
+#endif
+                };
+
+#if __EMBY__
+                var collection = this.collectionManager.CreateCollection(option);
+#else
+                var collection = await this.collectionManager.CreateCollectionAsync(option).ConfigureAwait(false);
+#endif
+            }
+
+            progress?.Report(100);
+        }
+
+        public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+        {
+            yield return new TaskTriggerInfo { Type = TaskTriggerInfo.TriggerWeekly, DayOfWeek = DayOfWeek.Sunday, TimeOfDayTicks = TimeSpan.FromHours(12).Ticks };
+        }
+    }
+}
