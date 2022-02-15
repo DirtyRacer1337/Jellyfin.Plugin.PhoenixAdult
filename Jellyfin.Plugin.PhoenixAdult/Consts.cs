6aeb72104c2bfaf4f9ebab10c98ca45393071cfe
DirtyRacer1337 <DirtyRacer@ya.ru>
2022-02-15 10:19:42 +0700

    Fix codestyle

--- Jellyfin.Plugin.PhoenixAdult/Consts.cs
+++ Jellyfin.Plugin.PhoenixAdult/Consts.cs
@@ -5,16 +5,15 @@ namespace PhoenixAdult
 {
     public static class Consts
     {
-#if __EMBY__
-        public static readonly string PluginInstance = "Emby.Plugins.PhoenixAdult";
+        public const string DatabaseUpdateURL = "https://api.github.com/repos/DirtyRacer1337/Jellyfin.Plugin.PhoenixAdult/contents/data";
 
-#else
         public const string SentryDSN = "https://8379d0e7cc2c45d8b1b6928ab8ff84c0@o1140949.ingest.sentry.io/6198587";
 
+#if __EMBY__
+        public static readonly string PluginInstance = "Emby.Plugins.PhoenixAdult";
+#else
         public static readonly string PluginInstance = "Jellyfin.Plugin.PhoenixAdult";
 #endif
-
-        public const string DatabaseUpdateURL = "https://api.github.com/repos/DirtyRacer1337/Jellyfin.Plugin.PhoenixAdult/contents/data";
 
         public static readonly string PluginVersion = $"{Plugin.Instance.Version.ToString()} build {Helper.GetLinkerTime(Assembly.GetAssembly(typeof(Plugin)))}";
     }
