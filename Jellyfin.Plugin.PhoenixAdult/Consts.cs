8509b2470f58cc4ad1178d5e05ed82c645421f18
DirtyRacer1337 <DirtyRacer@ya.ru>
2022-02-18 09:03:26 +0700

    Minor changes

--- Jellyfin.Plugin.PhoenixAdult/Consts.cs
+++ Jellyfin.Plugin.PhoenixAdult/Consts.cs
@@ -7,14 +7,14 @@ namespace PhoenixAdult
     {
         public const string DatabaseUpdateURL = "https://api.github.com/repos/DirtyRacer1337/Jellyfin.Plugin.PhoenixAdult/contents/data";
 
+#if __EMBY__
+        public const string PluginInstance = "Emby.Plugins.PhoenixAdult";
+#else
         public const string SentryDSN = "https://8379d0e7cc2c45d8b1b6928ab8ff84c0@o1140949.ingest.sentry.io/6198587";
 
-#if __EMBY__
-        public static readonly string PluginInstance = "Emby.Plugins.PhoenixAdult";
-#else
-        public static readonly string PluginInstance = "Jellyfin.Plugin.PhoenixAdult";
+        public const string PluginInstance = "Jellyfin.Plugin.PhoenixAdult";
 #endif
 
-        public static readonly string PluginVersion = $"{Plugin.Instance.Version.ToString()} build {Helper.GetLinkerTime(Assembly.GetAssembly(typeof(Plugin)))}";
+        public static readonly string PluginVersion = $"{Plugin.Instance.Version} build {Helper.GetLinkerTime(Assembly.GetAssembly(typeof(Plugin)))}";
     }
 }
